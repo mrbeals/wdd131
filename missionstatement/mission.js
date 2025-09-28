fd55a34887520f785db796aea85de92503834dd3
mrbeals <sbeals2@byui.edu>
2025-09-27 21:41:24 -0700

    Dark mode added

---
+++ missionstatement/mission.js
@@ -0,0 +1,24 @@
+const themeSelector = document.querySelector("select")
+const body = document.querySelector("body")
+function changeTheme() {
+// check to see what the current value of our select is.
+// The current value is conveniently found in themeSelector.value!
+if (themeSelector.value == "dark") {
+    body.classList.add("dark");
+    logo.src = "byui-logo_white.png";
+}
+else {
+    body.classList.remove("dark")
+    logo.src = "byui-logo_blue.webp";
+}
+// if the value is dark then:
+// add the dark class to the body
+// change the source of the logo img to point to the white logo.
+// otherwise
+// remove the dark class
+// make sure the logo src is the blue logo.
+}
+
+// add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
