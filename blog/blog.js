0c36349a497e2c8e75af87fb8bcf1463a26fd7b1
mrbeals <sbeals2@byui.edu>
2025-10-25 23:38:56 -0700

    This has been my favorite assigment

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,66 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
-] 
+]
+
+const left = document.querySelector(".left")
+const middle = document.querySelector(".middle")
+
+function createArticle(article) {
+	const book_addition = document.createElement('div');
+	book_addition.classList.add(`book${article.id}`);
+
+	const new_article = `
+		<p class="date">${article.date}</p>
+        <p class="age">${article.ages}</p>
+        <p class="genre">${article.genre}</p>
+        <p class="stars">${article.stars}</p>
+		`;
+
+	book_addition.innerHTML = new_article;
+	left.appendChild(book_addition);
+
+	const middle_addition = document.createElement('div');
+	middle_addition.classList.add(`book${article.id}`);
+
+	const middle_article = `
+		<h2 class="title">${article.title}</h2>
+    	<img src=${article.imgSrc} alt=${article.imgAlt} class="booking">
+    	<p class="description">${article.description}</p>
+	`;
+
+	middle_addition.innerHTML = middle_article;
+	middle.appendChild(middle_addition);
+
+	let min = article.id;
+	let max = article.id + 1;
+
+	const style = document.createElement('style');
+	document.head.appendChild(style);
+
+	style.sheet.insertRule(`
+		.book${article.id} {
+			grid-row: ${min}/${max};
+			height:700px;
+		}
+		`);
+	
+	
+	
+}
+
+articles.forEach(article => createArticle(article))
